0a77d00378c0ea1a32546f5b601dc11788ec254b
Денис <@LAPTOP-JOO03DUG>
2025-02-26 08:54:34 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encodeButton = new System.Windows.Forms.Button();
+            this.decodeButton = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.RichTextBox();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // encodeButton
+            // 
+            this.encodeButton.Location = new System.Drawing.Point(351, 164);
+            this.encodeButton.Name = "encodeButton";
+            this.encodeButton.Size = new System.Drawing.Size(141, 28);
+            this.encodeButton.TabIndex = 3;
+            this.encodeButton.Text = "зашифровать";
+            this.encodeButton.UseVisualStyleBackColor = true;
+            this.encodeButton.Click += new System.EventHandler(this.encodeButton_Click);
+            // 
+            // decodeButton
+            // 
+            this.decodeButton.Location = new System.Drawing.Point(351, 213);
+            this.decodeButton.Name = "decodeButton";
+            this.decodeButton.Size = new System.Drawing.Size(141, 28);
+            this.decodeButton.TabIndex = 4;
+            this.decodeButton.Text = "расшифровать";
+            this.decodeButton.UseVisualStyleBackColor = true;
+            this.decodeButton.Click += new System.EventHandler(this.decodeButton_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(12, 92);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(351, 22);
+            this.inputBox.TabIndex = 5;
+            this.inputBox.Text = "";
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(201, 275);
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.Size = new System.Drawing.Size(403, 145);
+            this.logBox.TabIndex = 6;
+            this.logBox.Text = "";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(430, 92);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(335, 22);
+            this.outputBox.TabIndex = 7;
+            this.outputBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.decodeButton);
+            this.Controls.Add(this.encodeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button encodeButton;
+        private System.Windows.Forms.Button decodeButton;
+        private System.Windows.Forms.RichTextBox inputBox;
+        private System.Windows.Forms.RichTextBox logBox;
+        private System.Windows.Forms.RichTextBox outputBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Code code = new Code();
+        public static string filein = "C:\\Users\\Денис\\Desktop\\in.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void encodeButton_Click(object sender, EventArgs e)
+        {
+            string str = code.StrCode(inputBox.Text);
+            string logstr = "Было " + inputBox.Text + " стало " + str;
+            logBox.Text += logstr + " \n";
+            
+            outputBox.Text = str;
+            
+            StreamWriter fin = new StreamWriter(filein, true);
+            fin.WriteLine(logstr);
+            fin.Close();
+        }
+
+        private void decodeButton_Click(object sender, EventArgs e)
+        {
+            string str = code.StrDecode(inputBox.Text);
+            
+            string logstr = "Было " + inputBox.Text + " стало " + str;
+            logBox.Text += logstr + " \n";
+            outputBox.Text = str;
+            StreamWriter fin = new StreamWriter(filein, true);
+            fin.WriteLine(logstr);
+            fin.Close();
+        }
+    }
+}
